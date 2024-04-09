9f488e441fb0c79ecafa935d67aea667beb816cf
DFatieieva <159715102+DFatieieva@users.noreply.github.com>
2024-04-09 14:26:04 +0300

    commit

---
+++ src/main/java/test/fit/model/Faculty.java
@@ -0,0 +1,37 @@
+package model;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Faculty {
+    private String name;
+    private List<Department> departments;
+
+    public Faculty(String name) {
+        this.name = name;
+        this.departments = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Department> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(List<Department> departments) {
+        this.departments = departments;
+    }
+
+    public void addDepartment(Department department) {
+        departments.add(department);
+    }
+
+    public void removeDepartment(Department department) {
+        departments.remove(department);
+    }
+}
