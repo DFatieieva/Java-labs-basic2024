37683980d9c487c305e908fc48833233befcb874
DFatieieva <159715102+DFatieieva@users.noreply.github.com>
2024-04-09 14:47:09 +0300

    commit

--- src/main/java/test/fit/model/Faculty.java
+++ src/main/java/test/fit/model/Faculty.java
@@ -1,10 +1,10 @@
-package model;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Faculty {
     private String name;
     private List<Department> departments;
+    private Human head;
 
     public Faculty(String name) {
         this.name = name;
@@ -33,5 +33,13 @@ public class Faculty {
 
     public void removeDepartment(Department department) {
         departments.remove(department);
+    }
+
+    public Human getHead() {
+        return head;
+    }
+
+    public void setHead(Human head) {
+        this.head = head;
     }
 }
