ef27c2f23cb2ae73268e61fee11eacf622806cfa
DFatieieva <159715102+DFatieieva@users.noreply.github.com>
2024-04-20 22:55:08 +0300

    commit

---
+++ src/main/java/test/fit/model/Human.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Human {
+    private String name;
+    private String surname;
+    private String patronymic;
+    private Sex gender;
+
+    public Human(String name, String surname, String patronymic, Sex gender) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.gender = gender;
+    }
+
+    // Гетери та сетери
+    public String getFirstName() {
+        return name;
+    }
+
+    public void setFirstName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return surname;
+    }
+
+    public void setLastName(String surname) {
+        this.surname = surname;
+    }
+
+    public String getMiddleName() {
+        return patronymic;
+    }
+
+    public void setMiddleName(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public Sex getSex() {
+        return gender;
+    }
+
+    public void setSex(Sex gender) {
+        this.gender = gender;
+    }
+    @Override
+    public String toString() {
+        return "{" + firstName + ", " + lastName + ", " + patronymic + '}';
+    }
+}
