ebce3a2b6b1d355a8d4ba44413b760db4769d34a
DFatieieva <159715102+DFatieieva@users.noreply.github.com>
2024-04-20 22:59:56 +0300

    commit

--- src/main/java/test/fit/model/Human.java
+++ src/main/java/test/fit/model/Human.java
@@ -1,52 +1,66 @@
 package model;
 
-public class Human {
-    private String name;
-    private String surname;
-    private String patronymic;
-    private Sex gender;
+import java.util.Objects;
 
-    public Human(String name, String surname, String patronymic, Sex gender) {
-        this.name = name;
-        this.surname = surname;
+public abstract class Human {
+    String firstName;
+    String lastName;
+    String patronymic;
+    Sex sex;
+
+    public Human(){
+    }
+
+    public Human(String firstName, String lastName, String patronymic, Sex sex) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.patronymic = patronymic;
-        this.gender = gender;
+        this.sex = sex;
     }
 
-    // Гетери та сетери
     public String getFirstName() {
-        return name;
+        return firstName;
     }
 
-    public void setFirstName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
-        return surname;
+        return lastName;
     }
 
-    public void setLastName(String surname) {
-        this.surname = surname;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
-    public String getMiddleName() {
+    public String getPatronymic() {
         return patronymic;
     }
 
-    public void setMiddleName(String patronymic) {
+    public void setPatronymic(String patronymic) {
         this.patronymic = patronymic;
     }
 
     public Sex getSex() {
-        return gender;
+        return sex;
     }
 
-    public void setSex(Sex gender) {
-        this.gender = gender;
+    public void setSex(Sex sex) {
+        this.sex = sex;
     }
+
     @Override
     public String toString() {
         return "{" + firstName + ", " + lastName + ", " + patronymic + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Human human = (Human) o;
+        return Objects.equals(firstName, human.firstName) && Objects.equals(lastName, human.lastName) &&
+                Objects.equals(patronymic, human.patronymic) && sex == human.sex;
     }
 }
