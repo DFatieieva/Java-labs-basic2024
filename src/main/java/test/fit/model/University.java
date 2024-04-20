0806f3291d20303d5bce917ee51139671ee8f27f
DFatieieva <159715102+DFatieieva@users.noreply.github.com>
2024-04-20 22:58:13 +0300

    commit

--- src/main/java/test/fit/model/University.java
+++ src/main/java/test/fit/model/University.java
@@ -1,11 +1,15 @@
-package lab_3.Model;
+package lab_4.Model;
+
+import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class University {
     private String name;
     private Human head;
+    @SerializedName("faculties")
     private List<Faculty> faculties;
 
     public University() {
@@ -44,5 +48,14 @@ public class University {
     @Override
     public String toString() {
         return "University{" + name + ", " + head + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        University that = (University) o;
+        return Objects.equals(name, that.name) && Objects.deepEquals(head, that.head) &&
+                Objects.deepEquals(faculties, that.faculties);
     }
 }
