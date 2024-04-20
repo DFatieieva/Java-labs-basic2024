188dabe91c5a5f5a82723dad8aa6ea99f69f8386
DFatieieva <159715102+DFatieieva@users.noreply.github.com>
2024-04-20 22:54:46 +0300

    commit

---
+++ src/main/java/test/fit/model/Group.java
@@ -0,0 +1,51 @@
+package model;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group {
+    private String name;
+    private Human head;
+    private List<Student> students;
+
+    public Group(String name, Human head) {
+        this.name = name;
+        this.head = head;
+        this.students = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Human getHead() {
+        return head;
+    }
+
+    public void setHead(Human head) {
+        this.head = head;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+    }
+}
+ @Override
+    public String toString() {
+
+        return String.format("Group{head=%s, students=%s}",head, students);
+
+    }
+
+}
